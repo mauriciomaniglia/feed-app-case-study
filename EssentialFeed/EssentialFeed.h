6dec564aed491d8a668c7e2acc45544c7114f0b7
Maurício Maniglia <manigliamauricio@gmail.com>
2019-08-06 22:18:47 -0300

    Created Essential Feed framework

---
+++ EssentialFeed/EssentialFeed.h
@@ -0,0 +1,19 @@
+//
+//  EssentialFeed.h
+//  EssentialFeed
+//
+//  Created by Mauricio Cesar Maniglia Junior on 06/08/19.
+//  Copyright © 2019 Mauricio Cesar Maniglia Junior. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for EssentialFeed.
+FOUNDATION_EXPORT double EssentialFeedVersionNumber;
+
+//! Project version string for EssentialFeed.
+FOUNDATION_EXPORT const unsigned char EssentialFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialFeed/PublicHeader.h>
+
+
