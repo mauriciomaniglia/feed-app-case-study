4ebfc328261bbb2f29d95d0205eb7ba4e60ad2a6
Maurício Maniglia <manigliamauricio@gmail.com>
2019-08-06 22:20:31 -0300

    Create FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Mauricio Cesar Maniglia Junior on 06/08/19.
+//  Copyright © 2019 Mauricio Cesar Maniglia Junior. All rights reserved.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
