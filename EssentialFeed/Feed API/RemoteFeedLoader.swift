a0dca0a761899b850085380d61ae6faf95a1b8a7
Maurício Maniglia <manigliamauricio@gmail.com>
2019-08-14 20:59:22 -0300

    Delivers invalidData error on non 200 HTTP responses

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 public protocol HTTPClient {
-    func get(from url: URL,  completion: @escaping (Error) -> Void)
+    func get(from url: URL,  completion: @escaping (Error?,  HTTPURLResponse?) -> Void)
 }
 
 public final class RemoteFeedLoader {
@@ -18,6 +18,7 @@ public final class RemoteFeedLoader {
     
     public enum Error: Swift.Error {
         case connectivity
+        case invalidData
     }
     
     public init(url: URL, client: HTTPClient) {
@@ -25,9 +26,13 @@ public final class RemoteFeedLoader {
         self.client = client
     }
     
-    public func load(completion: @escaping (Error) -> Void = { _ in }) {
-        client.get(from: url) { error in
-            completion(.connectivity)
+    public func load(completion: @escaping (Error) -> Void) {
+        client.get(from: url) { error, response in
+            if response != nil {
+                completion(.invalidData)
+            } else {
+                completion(.connectivity)
+            }
         }
     }
 }
--- EssentialFeedTests/RemoteFeedLoaderTest.swift
+++ EssentialFeedTests/RemoteFeedLoaderTest.swift
@@ -45,10 +45,19 @@ class RemoteFeedLoaderTest: XCTestCase {
         client.complete(with: clientError)
         
         XCTAssertEqual(capturedErrors, [.connectivity])
-            
-        }
-    
     }
+    
+    func test_load_deliversErrorOnNon200HTTPResponse() {
+        let (sut, client) = makeSUT()
+        
+        var capturedErrors = [RemoteFeedLoader.Error]()
+        sut.load { capturedErrors.append($0)}
+        client.complete(withStatusCode: 400)
+        
+        XCTAssertEqual(capturedErrors, [.invalidData])
+    }
+    
+}
     
     // MARK: Helpers
     
@@ -60,18 +69,28 @@ class RemoteFeedLoaderTest: XCTestCase {
     }
     
     class HTTPClientSpy: HTTPClient {
-        var messages = [ (url: URL, completion: (Error) -> Void) ]()
+        var messages = [ (url: URL, completion: (Error?, HTTPURLResponse?) -> Void) ]()
         
         var requestedURLs:  [URL] {
             return messages.map { $0.url }
         }
         
-        func get(from url: URL, completion: @escaping (Error) -> Void) {
+        func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void) {
             messages.append((url, completion))
         }
         
         func complete(with error: Error, at index: Int = 0) {
-            messages[index].completion(error)
+            messages[index].completion(error, nil)
+        }
+        
+        func complete(withStatusCode code: Int, at index: Int = 0) {
+            let response = HTTPURLResponse(
+                url: requestedURLs[index],
+                statusCode: code,
+                httpVersion: nil,
+                headerFields: nil
+            )
+            messages[index].completion(nil, response)
         }
     }
 
