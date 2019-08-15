c3e5b7920761361c7015c0e5ea978c4548dcb7c9
Maurício Maniglia <manigliamauricio@gmail.com>
2019-08-14 21:18:18 -0300

    Replace optional with enum with success/failure cases

--- EssentialFeedTests/RemoteFeedLoaderTest.swift
+++ EssentialFeedTests/RemoteFeedLoaderTest.swift
@@ -48,7 +48,7 @@ class RemoteFeedLoaderTest: XCTestCase {
     }
     
     func test_load_deliversErrorOnNon200HTTPResponse() {
-        let (sut, client) = makeSUT()        
+        let (sut, client) = makeSUT()
         let samples =  [199, 201, 300, 400, 500]
         
        samples.enumerated().forEach { index, code in
@@ -73,18 +73,18 @@ class RemoteFeedLoaderTest: XCTestCase {
     }
     
     class HTTPClientSpy: HTTPClient {
-        var messages = [ (url: URL, completion: (Error?, HTTPURLResponse?) -> Void) ]()
+        var messages = [ (url: URL, completion: (HTTPClientResult) -> Void) ]()
         
         var requestedURLs:  [URL] {
             return messages.map { $0.url }
         }
         
-        func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void) {
+        func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
             messages.append((url, completion))
         }
         
         func complete(with error: Error, at index: Int = 0) {
-            messages[index].completion(error, nil)
+            messages[index].completion(.failure(error))
         }
         
         func complete(withStatusCode code: Int, at index: Int = 0) {
@@ -93,8 +93,8 @@ class RemoteFeedLoaderTest: XCTestCase {
                 statusCode: code,
                 httpVersion: nil,
                 headerFields: nil
-            )
-            messages[index].completion(nil, response)
+            )!
+            messages[index].completion(.success(response))
         }
     }
 
