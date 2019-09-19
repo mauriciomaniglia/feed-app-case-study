e0fbbe4ce9b7bd913c618caf45b91fa4e9a36476
Maurício Maniglia <manigliamauricio@gmail.com>
2019-09-18 21:47:41 -0300

    Move `FeedStore` to its own file

---
+++ EssentialFeed/Feed Cache/FeedStore.swift
@@ -0,0 +1,17 @@
+//
+//  FeedStore.swift
+//  EssentialFeed
+//
+//  Created by Mauricio Cesar Maniglia Junior on 18/09/19.
+//  Copyright © 2019 Mauricio Cesar Maniglia Junior. All rights reserved.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items:[FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
