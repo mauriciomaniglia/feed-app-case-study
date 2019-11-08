8d5ad427147b1b0b5900a53b16069e89555e526f
Maurício Maniglia <manigliamauricio@gmail.com>
2019-11-08 10:44:17 -0300

    Proved that `CoreDataFeedStore` side-effects run serially

--- EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -106,7 +106,30 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     }
 
     func test_storeSideEffects_runSerially() {
-
+        let sut = makeSUT()
+        var completedOperationInOrder = [XCTestExpectation]()
+        
+        let op1 = expectation(description: "Operation 1")
+        sut.insert(uniqueImageFeed().local, timestamp: Date()) { _ in
+            completedOperationInOrder.append(op1)
+            op1.fulfill()
+        }
+        
+        let op2 = expectation(description: "Operation 2")
+        sut.deleteCachedFeed { _ in
+            completedOperationInOrder.append(op2)
+            op2.fulfill()
+        }
+        
+        let op3 = expectation(description: "Operation 3")
+        sut.insert(uniqueImageFeed().local, timestamp: Date()) { _ in
+            completedOperationInOrder.append(op3)
+            op3.fulfill()
+        }
+        
+        waitForExpectations(timeout: 5.0)
+        
+        XCTAssertEqual(completedOperationInOrder, [op1, op2, op3], "Expected side-effects to run serially but operations finished in the wrong order") 
     }
     
     // - MARK: Helpers
