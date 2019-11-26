3d8d25bf16ee03b72580276366a2b79909dae777
Maurício Maniglia <manigliamauricio@gmail.com>
2019-11-26 19:37:45 -0300

    Render loaded feed on successful load completion

---
+++ EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by zupper on 26/11/19.
+//  Copyright © 2019 Mauricio Cesar Maniglia Junior. All rights reserved.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
