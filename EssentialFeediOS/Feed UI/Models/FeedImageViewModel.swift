1a0ddaa64d1f602134d3488381482b7aa9fcf890
Mauricio Maniglia <manigliamauricio@gmail.com>
2020-01-12 20:51:45 -0300

    Decouple `FeedImageViewModel` from `UIKit` by creating a transformation closure that converts an image `Data` value into a generic `Image` type. When composing with a UIKit user interface, we inject a closure to transform the image `Data` into `UIImage`.

--- EssentialFeediOS/Feed UI/Models/FeedImageViewModel.swift
+++ EssentialFeediOS/Feed UI/Models/FeedImageViewModel.swift
@@ -10,53 +10,55 @@ import Foundation
 import UIKit
 import EssentialFeed
 
- final class FeedImageViewModel {
-   typealias Observer<T> = (T) -> Void
+ final class FeedImageViewModel<Image> {
+    typealias Observer<T> = (T) -> Void
 
-   private var task: FeedImageDataLoaderTask?
-   private let model: FeedImage
-   private let imageLoader: FeedImageDataLoader
+    private var task: FeedImageDataLoaderTask?
+    private let model: FeedImage
+    private let imageLoader: FeedImageDataLoader
+    private let imageTransformer: (Data) -> Image?
 
-   init(model: FeedImage, imageLoader: FeedImageDataLoader) {
-       self.model = model
-       self.imageLoader = imageLoader
-   }
+    init(model: FeedImage, imageLoader: FeedImageDataLoader, imageTransformer: @escaping (Data) -> Image?) {
+        self.model = model
+        self.imageLoader = imageLoader
+        self.imageTransformer = imageTransformer
+    }
 
-   var description: String? {
+    var description: String? {
        return model.description
-   }
+    }
 
-   var location: String?  {
+    var location: String?  {
        return model.location
-   }
+    }
 
-   var hasLocation: Bool {
+    var hasLocation: Bool {
        return location != nil
-   }
+    }
 
-   var onImageLoad: Observer<UIImage>?
-   var onImageLoadingStateChange: Observer<Bool>?
-   var onShouldRetryImageLoadStateChange: Observer<Bool>?
+    var onImageLoad: Observer<Image>?
+    var onImageLoadingStateChange: Observer<Bool>?
+    var onShouldRetryImageLoadStateChange: Observer<Bool>?
 
-   func loadImageData() {
+    func loadImageData() {
        onImageLoadingStateChange?(true)
        onShouldRetryImageLoadStateChange?(false)
        task = imageLoader.loadImageData(from: model.url) { [weak self] result in
            self?.handle(result)
        }
-   }
+    }
 
-   private func handle(_ result: FeedImageDataLoader.Result) {
-       if let image = (try? result.get()).flatMap(UIImage.init) {
+    private func handle(_ result: FeedImageDataLoader.Result) {
+       if let image = (try? result.get()).flatMap(imageTransformer) {
            onImageLoad?(image)
        } else {
            onShouldRetryImageLoadStateChange?(true)
        }
        onImageLoadingStateChange?(false)
-   }
+    }
 
-   func cancelImageDataLoad() {
+    func cancelImageDataLoad() {
        task?.cancel()
        task = nil
-   }
+    }
 }
