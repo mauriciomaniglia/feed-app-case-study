302b44208759c304c19ae944140b6bc825108426
Mauricio Maniglia <manigliamauricio@gmail.com>
2020-01-12 19:25:06 -0300

    Remove mutable state from `FeedViewModel`. The `FeedViewModel` only needs to forward state changes, so state is only transient.

--- EssentialFeediOS/Feed UI/Models/FeedViewModel.swift
+++ EssentialFeediOS/Feed UI/Models/FeedViewModel.swift
@@ -10,6 +10,7 @@ import Foundation
 import EssentialFeed
 
 final class FeedViewModel {
+    typealias Observer<T> = (T) -> Void
     
     private let feedLoader: FeedLoader
     
@@ -17,20 +18,16 @@ final class FeedViewModel {
         self.feedLoader = feedLoader
     }
     
-    var onChange: ((FeedViewModel) -> Void)?
-    var onFeedLoad: (([FeedImage]) -> Void)?
-    
-    private(set) var isLoading: Bool = false {
-        didSet { onChange?(self) }
-    }
+    var onLoadingStateChange: Observer<Bool>?
+    var onFeedLoad: Observer<[FeedImage]>?
     
     func loadFeed() {
-        isLoading = true
+        onLoadingStateChange?(true)
         feedLoader.load { [weak self] result in
             if let feed = try? result.get() {
                 self?.onFeedLoad?(feed)
             }
-            self?.isLoading = false
+            self?.onLoadingStateChange?(false)
         }
     }
 }
