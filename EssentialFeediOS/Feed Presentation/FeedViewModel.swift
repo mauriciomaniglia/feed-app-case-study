6b3a4329ec0a99ffdfb8501ae2efc78bdea4e211
Mauricio Maniglia <manigliamauricio@gmail.com>
2020-01-15 06:59:23 -0300

    Move ViewModels to separate files

---
+++ EssentialFeediOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Mauricio Maniglia on 15/01/20.
+//  Copyright © 2020 Mauricio Cesar Maniglia Junior. All rights reserved.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
