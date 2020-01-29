9244f6c1b52c58025f4d0d1117e5e6896bae06bf
Mauricio Maniglia <manigliamauricio@gmail.com>
2020-01-29 16:54:48 -0300

    Move `FeedErrorViewModel` to separate file

---
+++ EssentialFeediOS/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  FeedErrorViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Mauricio Maniglia on 29/01/20.
+//  Copyright © 2020 Mauricio Cesar Maniglia Junior. All rights reserved.
+//
+
+struct FeedErrorViewModel {
+    let message: String?
+}
