4d79c1365af21ecfc409cad749a0807d1ac3a1b1
Mauricio Maniglia <manigliamauricio@gmail.com>
2020-01-30 06:45:04 -0300

    Move `FeedViewModel` to separate file

--- EssentialFeed/Feed Presentation/FeedPresenter.swift
+++ EssentialFeed/Feed Presentation/FeedPresenter.swift
@@ -8,10 +8,6 @@
 
 import Foundation
 
-public struct FeedViewModel {
-    public let feed: [FeedImage]
-}
-
 public protocol FeedView {
     func display(_ viewModel: FeedViewModel)
 }
---
+++ EssentialFeed/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by Mauricio Maniglia on 30/01/20.
+//  Copyright © 2020 Mauricio Cesar Maniglia Junior. All rights reserved.
+//
+
+import Foundation
+
+public struct FeedViewModel {
+    public let feed: [FeedImage]
+}
