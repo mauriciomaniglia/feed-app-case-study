6003938de5cbd90feb07c7537e25424d8da2c4ba
Mauricio Maniglia <manigliamauricio@gmail.com>
2020-01-30 06:46:14 -0300

    Move `FeedLoadingViewModel` to separate file

---
+++ EssentialFeed/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Mauricio Maniglia on 30/01/20.
+//  Copyright © 2020 Mauricio Cesar Maniglia Junior. All rights reserved.
+//
+
+import Foundation
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
