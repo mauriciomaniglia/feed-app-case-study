dbab7005c445bf5ab3b9d006b041d6141092ca19
mauriciomaniglia <manigliamauricio@gmail.com>
2020-02-03 11:53:39 -0300

    Removed unnecessary import from `FeedLocalizationTests` since this class is agnostic of platform

--- EssentialFeedTests/Feed Presentation/FeedLocalizationTests.swift
+++ EssentialFeedTests/Feed Presentation/FeedLocalizationTests.swift
@@ -9,8 +9,6 @@
 import XCTest
 import EssentialFeed
 
-@testable import EssentialFeediOS
-
  final class FeedLocalizationTests: XCTestCase {
 
    func test_localizedStrings_haveKeysAndValuesForAllSupportedLocalizations() {
