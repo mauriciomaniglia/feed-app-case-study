204961e9f27ae23fc6f1bc62f58719a4e1f4a0d3
mauriciomaniglia <manigliamauricio@gmail.com>
2020-02-18 14:25:04 -0300

    Extract `uniqueFeed` factory helper into a shared scope to remove duplication

--- EssentialApp/EssentialAppTests/Helpers/SharedTestHelpers.swift
+++ EssentialApp/EssentialAppTests/Helpers/SharedTestHelpers.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import EssentialFeed
 
 func anyNSError() -> NSError {
     return NSError(domain: "any error", code: 0)
@@ -19,4 +20,8 @@ func anyURL() -> URL {
 
 func anyData() -> Data {
     return Data("any data".utf8)
+}
+
+func uniqueFeed() -> [FeedImage] {
+    return [FeedImage(id: UUID(), description: "any", location: "any", url: anyURL())]
 }
