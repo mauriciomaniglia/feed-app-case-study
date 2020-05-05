df8591d8935c5ff452c5ec1419e2f0203c22583d
mauriciomaniglia <manigliamauricio@gmail.com>
2020-05-05 08:13:48 -0300

    Fix tuple pattern issue related with xcode upgrade to 11.4
    https://developer.apple.com/documentation/xcode_release_notes/xcode_11_4_release_notes
    
    `Code that relies on the compiler automatically tupling a pattern may lead to a compiler error when upgrading to Xcode 11.4, even though the code compiled before. (58425942)`

--- EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -29,7 +29,7 @@ public final class RemoteFeedLoader: FeedLoader {
             guard self != nil else { return }
             
             switch (result) {
-            case let .success(data, response):
+            case let .success((data, response)):                
                 completion(RemoteFeedLoader.map(data, response: response))
             case .failure:
                 completion(.failure(Error.connectivity))
